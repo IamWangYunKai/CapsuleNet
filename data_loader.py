8f048d33ff69dafde28f16c6712bee6aad649e48
IamWangYunKai <615349118@163.com>
2019-12-16 20:34:57 +0800

    fork from jindongwang/Pytorch-CapsuleNet

---
+++ data_loader.py
@@ -0,0 +1,40 @@
+import torch
+from torchvision import datasets, transforms
+
+
+class Dataset:
+    def __init__(self, dataset, _batch_size):
+        super(Dataset, self).__init__()
+        if dataset == 'mnist':
+            dataset_transform = transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Normalize((0.1307,), (0.3081,))
+            ])
+
+            train_dataset = datasets.MNIST('./data/mnist', train=True, download=True,
+                                           transform=dataset_transform)
+            test_dataset = datasets.MNIST('./data/mnist', train=False, download=True,
+                                          transform=dataset_transform)
+
+            self.train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=_batch_size, shuffle=True, num_workers=16)
+            self.test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=_batch_size, shuffle=False, num_workers=16)
+
+        elif dataset == 'cifar10':
+            data_transform = transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+            ])
+            train_dataset = datasets.CIFAR10(
+                './data/cifar', train=True, download=True, transform=data_transform)
+            test_dataset = datasets.CIFAR10(
+                './data/cifar', train=False, download=True, transform=data_transform)
+
+            self.train_loader = torch.utils.data.DataLoader(
+                train_dataset, batch_size=_batch_size, shuffle=True)
+
+            self.test_loader = torch.utils.data.DataLoader(
+                test_dataset, batch_size=_batch_size, shuffle=False)
+        elif dataset == 'office-caltech':
+            pass
+        elif dataset == 'office31':
+            pass
---
+++ run.py
@@ -0,0 +1,156 @@
+import numpy as np
+from datetime import datetime
+import random
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+from capsnet import CapsNet
+from data_loader import Dataset
+from tqdm import tqdm
+
+random.seed(datetime.now())
+torch.manual_seed(999)
+torch.cuda.manual_seed(999)
+torch.set_num_threads(16)
+
+USE_CUDA = True if torch.cuda.is_available() else False
+BATCH_SIZE = 64
+N_EPOCHS = 100
+LEARNING_RATE = 3e-4
+MOMENTUM = 0.9
+
+'''
+Config class to determine the parameters for capsule net
+'''
+
+
+class Config:
+    def __init__(self, dataset='mnist'):
+        if dataset == 'mnist':
+            # CNN (cnn)
+            self.cnn_in_channels = 1
+            self.cnn_out_channels = 256
+            self.cnn_kernel_size = 9
+
+            # Primary Capsule (pc)
+            self.pc_num_capsules = 8
+            self.pc_in_channels = 256
+            self.pc_out_channels = 32
+            self.pc_kernel_size = 9
+            self.pc_num_routes = 32 * 6 * 6
+
+            # Digit Capsule (dc)
+            self.dc_num_capsules = 10
+            self.dc_num_routes = 32 * 6 * 6
+            self.dc_in_channels = 8
+            self.dc_out_channels = 16
+
+            # Decoder
+            self.input_width = 28
+            self.input_height = 28
+
+        elif dataset == 'cifar10':
+            # CNN (cnn)
+            self.cnn_in_channels = 3
+            self.cnn_out_channels = 256
+            self.cnn_kernel_size = 9
+
+            # Primary Capsule (pc)
+            self.pc_num_capsules = 8
+            self.pc_in_channels = 256
+            self.pc_out_channels = 32
+            self.pc_kernel_size = 9
+            self.pc_num_routes = 32 * 8 * 8
+
+            # Digit Capsule (dc)
+            self.dc_num_capsules = 10
+            self.dc_num_routes = 32 * 8 * 8
+            self.dc_in_channels = 8
+            self.dc_out_channels = 16
+
+            # Decoder
+            self.input_width = 32
+            self.input_height = 32
+
+        elif dataset == 'your own dataset':
+            pass
+
+
+def train(model, optimizer, train_loader, epoch):
+    capsule_net = model
+    capsule_net.train()
+    n_batch = len(list(enumerate(train_loader)))
+    total_loss = 0
+    for batch_id, (data, target) in enumerate(tqdm(train_loader)):
+
+        target = torch.sparse.torch.eye(10).index_select(dim=0, index=target)
+        data, target = Variable(data), Variable(target)
+
+        if USE_CUDA:
+            data, target = data.cuda(), target.cuda()
+
+        optimizer.zero_grad()
+        output, reconstructions, masked = capsule_net(data)
+        loss = capsule_net.loss(data, output, target, reconstructions)
+        if torch.isnan(loss):
+            print('FFFFFFFFFFFFFFFuck\n\n\n\n\n')
+
+        loss.backward()
+        torch.nn.utils.clip_grad_value_(capsule_net.parameters(), clip_value=20)
+        optimizer.step()
+        correct = sum(np.argmax(masked.data.cpu().numpy(), 1) == np.argmax(target.data.cpu().numpy(), 1))
+        train_loss = loss.item()
+        total_loss += train_loss
+        if batch_id % 100 == 0:
+            tqdm.write("Epoch: [{}/{}], Batch: [{}/{}], train accuracy: {:.6f}, loss: {:.6f}".format(
+                epoch,
+                N_EPOCHS,
+                batch_id + 1,
+                n_batch,
+                correct / float(BATCH_SIZE),
+                train_loss / float(BATCH_SIZE)
+                ))
+    tqdm.write('Epoch: [{}/{}], train loss: {:.6f}'.format(epoch,N_EPOCHS,total_loss / len(train_loader.dataset)))
+
+
+def test(capsule_net, test_loader, epoch):
+    capsule_net.eval()
+    test_loss = 0
+    correct = 0
+    for batch_id, (data, target) in enumerate(test_loader):
+
+        target = torch.sparse.torch.eye(10).index_select(dim=0, index=target)
+        data, target = Variable(data), Variable(target)
+
+        if USE_CUDA:
+            data, target = data.cuda(), target.cuda()
+
+        output, reconstructions, masked = capsule_net(data)
+        loss = capsule_net.loss(data, output, target, reconstructions)
+        test_loss += loss.item()
+        correct += sum(np.argmax(masked.data.cpu().numpy(), 1) ==
+                       np.argmax(target.data.cpu().numpy(), 1))
+
+    tqdm.write(
+        "Epoch: [{}/{}], test accuracy: {:.6f}, loss: {:.6f}".format(epoch, N_EPOCHS, correct / len(test_loader.dataset),
+                                                                  test_loss / len(test_loader)))
+
+
+if __name__ == '__main__':
+    dataset = 'cifar10'
+    #dataset = 'mnist'
+    config = Config(dataset)
+    mnist = Dataset(dataset, BATCH_SIZE)
+
+    capsule_net = CapsNet(config)
+    capsule_net = torch.nn.DataParallel(capsule_net)
+    if USE_CUDA:
+        capsule_net = capsule_net.cuda()
+    capsule_net = capsule_net.module
+
+    optimizer = torch.optim.Adam(capsule_net.parameters())
+
+    for e in range(1, N_EPOCHS + 1):
+        train(capsule_net, optimizer, mnist.train_loader, e)
+        test(capsule_net, mnist.test_loader, e)
